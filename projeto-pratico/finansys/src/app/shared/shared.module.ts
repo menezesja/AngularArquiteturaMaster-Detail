a376fbb23b1a21e487c01539639452843a203037
menezesja <santmjad@gmail.com>
2023-11-08 15:08:54 -0400

    adding RouterModule to SharedModule && improving page components with BreadCrumbComponent

--- projeto-pratico/finansys/src/app/shared/shared.module.ts
+++ projeto-pratico/finansys/src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from "@angular/router";
 import { BreadCrumbComponent } from './components';
 
 @NgModule({
@@ -9,11 +10,13 @@ import { BreadCrumbComponent } from './components';
   ],
   imports: [
     CommonModule,
-    ReactiveFormsModule 
+    ReactiveFormsModule,
+    RouterModule 
   ], 
   exports:[
     CommonModule,
     ReactiveFormsModule,
+    RouterModule,
     BreadCrumbComponent
   ]
 })
