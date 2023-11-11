35197052fcc498717be932137c4ec980d257c83d
menezesja <santmjad@gmail.com>
2023-11-11 14:45:28 -0400

    adding reports base

---
+++ projeto-pratico/finansys/src/app/pages/reports/index.ts
@@ -0,0 +1,3 @@
+export * from './reports-routing.module';
+export * from './reports.module';
+export * from './reports';
---
+++ projeto-pratico/finansys/src/app/pages/reports/reports.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { SharedModule } from 'src/app/shared';
+
+import { ReportsRoutingModule } from './reports-routing.module';
+import { ReportsComponent } from './reports/reports.component';
+
+import { ChartModule } from "primeng/chart";
+
+@NgModule({
+  declarations: [
+    ReportsComponent
+  ],
+  imports: [
+    SharedModule,
+    ReportsRoutingModule,
+    ChartModule
+  ]
+})
+export class ReportsModule { }
