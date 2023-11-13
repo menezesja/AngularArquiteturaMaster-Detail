4726e8dab316cbb4fa84bdffb8dd25003bfcfab4
menezesja <santmjad@gmail.com>
2023-11-13 08:21:27 -0400

    improving Reports

--- projeto-pratico/finansys/src/app/pages/entries/shared/entry.service.ts
+++ projeto-pratico/finansys/src/app/pages/entries/shared/entry.service.ts
@@ -1,9 +1,12 @@
 import { Injectable, Injector } from '@angular/core';
-import { BaseResourceService } from 'src/app/shared/services';
-import { Entry } from './entry.model';
-import { CategoryService } from '../../categories';
 
-import { Observable, catchError, mergeMap} from 'rxjs';
+import { Observable, catchError, mergeMap, map} from 'rxjs';
+
+import { BaseResourceService } from 'src/app/shared/services';
+import { CategoryService } from '../../categories';
+import { Entry } from './entry.model';
+
+import * as moment from "moment";
 
 @Injectable({
   providedIn: 'root'
@@ -22,6 +25,12 @@ export class EntryService extends BaseResourceService <Entry> {
     return this.setCategoryAndSendToServer(entry, super.update.bind(this));
   }
 
+  getByMonthAndYear(month: number, year: number): Observable<Entry[]> {
+    return this.getAll().pipe(
+      map(entries => this.filterByMonthAndYear(entries, month, year))
+    )
+  }
+
   private setCategoryAndSendToServer(entry: Entry, sendFn: any): Observable<Entry>{
     return this.categoryService.getByID(entry.categoryId).pipe(
       mergeMap(category => {
@@ -30,5 +39,18 @@ export class EntryService extends BaseResourceService <Entry> {
       }),
       catchError(this.handlerError)
     );
+  }
+
+  private filterByMonthAndYear(entries: Entry[], month: number, year: number) {
+    return entries.filter(entry => {
+      const entryDate = moment(entry.date, "DD/MM/YYYY");
+      const monthMatches = entryDate.month() + 1 == month;
+      const yearMatches = entryDate.year() == year;
+
+      if(monthMatches && yearMatches) 
+        return entry;
+      else
+        return [];
+    })
   }
 }
